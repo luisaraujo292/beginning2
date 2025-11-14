13a471920bf873626e990fd1a47f855d4f9e0e1e
Luís Sérgio <luissergioaraujo@yahoo.com.br>
2025-11-14 10:38:50 -0300

    Fix post.

--- src/app/post/page.js
+++ src/app/post/page.js
@@ -6,9 +6,18 @@ function UserProfile(props) {
     <div>
       <h2>{props.user.name}</h2>
       <p>Email: {props.user.email}</p>
-      <p>Age: {props.user.name}</p>
+      <p>Age: {props.user.age}</p>
     </div>
   );
 }
 
-export default UserProfile;
+function App() {
+  const user = {
+    name:"Luís Sérgio",
+    email:"luisaraujo292@yahoo.com",
+    age:"67"
+  }
+  return <UserProfile user={user}></UserProfile>
+}
+
+export default App;
