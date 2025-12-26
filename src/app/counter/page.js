96ab19f11803d3ac057730fb8870e8d7ece62f68
Luís Sérgio <luissergioaraujo@yahoo.com.br>
2025-12-26 14:53:15 -0300

    Include !

--- src/app/counter/page.js
+++ src/app/counter/page.js
@@ -7,7 +7,7 @@ function Counter(){
   const [count,setCount] = useState(0);
   return (
     <div>
-      <p>You clicked {count} times</p>
+      <p>You clicked {count} times!</p>
       <button onClick={() => setCount(count+1)}>Click me</button>
     </div>
   );
